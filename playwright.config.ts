ec03898020f3b3e7082f392683841594bbf06f41
Muhindhar <muhindharsv27@gmail.com>
2026-06-24 17:18:59 +0530

    added playwright task

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot:'on-first-failure',
+    screenshot:'only-on-failure',
     video:'retain-on-failure',
     headless:true,
   },
